53be13f8c203d4a2388053b0a78edd7b6a625f84
KaitoMasuda <37993351+masukai@users.noreply.github.com>
2021-06-16 10:10:59 +0900

    20210616_update

---
+++ Python/ABC157/C.py
@@ -0,0 +1,29 @@
+N, M = map(int, input().split())
+sc = [list(map(int, input().split())) for sc in range(M)]
+
+num = ["A"] * N
+counter = [0] * N
+ans = 0
+for i in range(M):
+    if (num[sc[i][0] - 1] != sc[i][1]):
+        if (counter[sc[i][0] - 1] < 1):
+            num[sc[i][0] - 1] = sc[i][1]
+            counter[sc[i][0] - 1] += 1
+        else:
+            ans = -1
+            break
+    else:
+        continue
+
+for i in range(N):
+    if (num[i] == "A"):
+        num[i] = 0
+
+if (ans != -1):
+    check = int(''.join(map(str, num)))
+    if (len(str(check)) == N):
+        ans = check
+    else:
+        ans = -1
+
+print(ans)
