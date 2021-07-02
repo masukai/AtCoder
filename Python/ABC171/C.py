c033d3605afab57ff3172aaee125b00f445fca3c
KaitoMasuda <37993351+masukai@users.noreply.github.com>
2021-07-02 09:02:33 +0900

    20210702_update

---
+++ Python/ABC171/C.py
@@ -0,0 +1,14 @@
+N = int(input())
+
+
+def num2alpha(num):
+    if num <= 26:
+        return chr(64 + num)
+    elif num % 26 == 0:
+        return num2alpha(num // 26 - 1) + chr(90)
+    else:
+        return num2alpha(num // 26) + chr(64 + num % 26)
+
+
+ans = num2alpha(N)
+print(ans.lower())
